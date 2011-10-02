bc937933fb8ae41750b0c04b64d339ae025cfc55
igorknop <igorknop@gmail.com>
2011-10-02 09:40:23 -0300

    Adding a beforeEach() on specs.
    Clean up fsa creation on every spec.

--- spec/FiniteStateAutomataSpec.js
+++ spec/FiniteStateAutomataSpec.js
@@ -1,31 +1,30 @@
 describe ("Finite State Automata", function(){
+   var fsa;
+   beforeEach(function(){
+      fsa = new FiniteStateAutomata();
+   });
    it("Should have a state on his state list after add a new state",function(){
-      var fsa = new FiniteStateAutomata();
       fsa.addState("1");
       expect(fsa.getState("1")).toBeDefined();
       expect(fsa.states["1"]).toBeDefined();
    });
 
    it("Should have a field to hold initial state", function(){
-      var fsa = new FiniteStateAutomata();
       fsa.addState("1");
       fsa.setInitialState("1");
       expect(fsa.initialState).toEqual("1");
    });
 
    it("Should throw an exception when set a the initial state to inexistant one", function(){
-      var fsa = new FiniteStateAutomata();
       expect(function(){ fsa.setInitialState("1") }).toThrow();
    });
 
    it("Should throw an exception  when a adding a already existant state", function(){
-      var fsa = new FiniteStateAutomata();
       fsa.addState(1);
       expect(function(){ fsa.addState(1) }).toThrow();
    });
 
    it("Should set the initial state to first added state when adding a state", function(){
-      var fsa = new FiniteStateAutomata();
       fsa.addState(1);
       expect(fsa.getInitialState()).toEqual(1);
       fsa.addState(2);
@@ -33,17 +32,14 @@ describe ("Finite State Automata", function(){
    });
 
    it("Should have initial state null when created", function(){
-      var fsa = new FiniteStateAutomata();
       expect(fsa.getInitialState()).toBeNull();   
    });
 
    it("Should have final state null when created", function(){
-      var fsa = new FiniteStateAutomata();
       expect(fsa.getFinalState()).toBeNull();   
    });
 
    it("Should set the final state to last added state when adding a state", function(){
-      var fsa = new FiniteStateAutomata();
       fsa.addState(1);
       expect(fsa.getFinalState()).toEqual(1);
       fsa.addState(2);
@@ -51,24 +47,20 @@ describe ("Finite State Automata", function(){
    });
 
    it("Should throw an exception when set the final state to inexistant one", function(){
-      var fsa = new FiniteStateAutomata();
       expect(function(){ fsa.setFinalState("1") }).toThrow();
    });
    
    it("Should have a empty transition list when created", function(){
-      var fsa = new FiniteStateAutomata();
       expect(fsa.transitions.length).toEqual(0);
    });
 
    it("Should throw an exception when setting a transition to an inexistant state", function(){
-      var fsa = new FiniteStateAutomata();
       fsa.addState(1);
       fsa.addState(2);
       expect(function(){fsa.addTransition(1,3,"t")}).toThrow();
    });
 
    it("Should return an array when looking for existing transitions", function(){
-      var fsa = new FiniteStateAutomata();
       fsa.addState(1);
       fsa.addState(2);
       fsa.addTransition(1, 2, "a");
