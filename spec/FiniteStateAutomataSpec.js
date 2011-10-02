253056dc9a9224f9b50e6c9ba5bc17a0bca86ff8
igorknop <igorknop@gmail.com>
2011-10-02 08:13:56 -0300

    Adding new methods to FiniteStateAutomata
    Adding a method to add states.
    Adding a method to set initial State.

--- spec/FiniteStateAutomataSpec.js
+++ spec/FiniteStateAutomataSpec.js
@@ -1,9 +1,27 @@
 describe ("Automato de Estados Finitos", function(){
-   
+   it("Deve ter um estado na sua lista após adicionar um novo estado",function(){
+      var fsa = new FiniteStateAutomata();
+      fsa.addState("1");
+      expect(fsa.getState("1")).toBeDefined();
+      expect(fsa.states["1"]).toBeDefined();
+   });
+
    it("Deve possuir um campo para guardar o estado inicial", function(){
       var fsa = new FiniteStateAutomata();
+      fsa.addState("1");
       fsa.setInitialState("1");
       expect(fsa.initialState).toEqual("1");
+   });
+
+   it("Deve disparar uma exceção se o estado inicial for inexistente", function(){
+      var fsa = new FiniteStateAutomata();
+      expect(function(){ fsa.setInitialState("1") }).toThrow();
+   });
+
+   it("Deve disparar uma exceção se o estado já foi definido anteriormente", function(){
+      var fsa = new FiniteStateAutomata();
+      fsa.addState(1);
+      expect(function(){ fsa.addState(1) }).toThrow();
    });
 
 });
--- src/FiniteStateAutomata.js
+++ src/FiniteStateAutomata.js
@@ -1,9 +1,26 @@
 function FiniteStateAutomata(){
-
+   this.states = new Array();
+   this.initialState = null;
 }
 
-FiniteStateAutomata.prototype.initialState = null;
-FiniteStateAutomata.prototype.setInitialState = function (initialState) {
-   this.initialState = initialState;
+FiniteStateAutomata.prototype.setInitialState = function (state) {
+   if (this.states[state]===undefined){
+      throw new ReferenceError("Initial value should Exist!");
+   }
+   this.initialState = state;
+};
+
+FiniteStateAutomata.prototype.addState = function (state) {
+   if (this.states[state]!==undefined){
+      throw new ReferenceError("State alread exist!");
+   }
+   this.states[state] = state;
+};
+
+FiniteStateAutomata.prototype.getState = function (state) {
+   if (this.states[state]===undefined){
+      throw new ReferenceError("State is not defined!");
+   }
+   return this.states[state];
 };
 
