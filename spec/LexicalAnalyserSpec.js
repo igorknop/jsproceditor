df032139fe45fd9731472ee52ae5bbaf1ff3ecba
igorknop <igorknop@gmail.com>
2011-10-05 03:06:53 -0300

    Creating new Lexical Analyser basic behaviour.

--- spec/LexicalAnalyserSpec.js
+++ spec/LexicalAnalyserSpec.js
@@ -1,4 +1,5 @@
 describe("Lexical Analyser", function(){
+      /*
       it("Should throw when there is a 0 in first state", function(){
          var fsm = new FiniteStateMachine();
          fsm.addState(1);
@@ -33,6 +34,68 @@ describe("Lexical Analyser", function(){
          expect(la.isLexicalCorrect(" aa")).toBeTruthy();
          expect(la.isLexicalCorrect(" xa")).toBeFalsy();
 
+      });*/
+
+      it("It should throw if machine isnt a FiniteStateMachine",function(){
+         var la = new LexicalAnalyser();
+         expect(function(){la.setMachine("string")}).toThrow();
       });
 
+      it("It should have an empty text when created", function(){
+         var la = new LexicalAnalyser();
+         expect(la.getText()).toBe("")
+      });
+
+      it("It should have a non empty text when setting the text", function(){
+         var la = new LexicalAnalyser();
+         la.setText("(a|b);c")
+         expect(la.getText()).toBe("(a|b);c");
+      });
+
+      it("It should return 0 as First Symbol Position when created", function(){
+         var la = new LexicalAnalyser();
+         expect(la.getFirstSymbolPosition()).toBe(0);
+      });
+
+      it("It should return 0 as Last Symbol Position when created", function(){
+         var la = new LexicalAnalyser();
+         expect(la.getLastSymbolPosition()).toBe(0);
+      });
+
+      it("It should have Last an First Symbol Position equals when setting first symbol position", function(){
+         var la = new LexicalAnalyser();
+         la.setText("(a|b);c");
+         la.setFirstSymbolPosition(3);
+         expect(la.getFirstSymbolPosition()).toBe(3);
+         expect(la.getLastSymbolPosition()).toBe(3);
+      });
+
+      it("it should throw when setting a first symbol position do a negative or greater than text size", function(){
+         var la = new LexicalAnalyser();
+         la.setText("(a|b);c");
+         expect(function(){la.setFirstSymbolPosition(-3)}).toThrow();
+         expect(function(){la.setFirstSymbolPosition(13)}).toThrow();
+      });
+
+      it("It should return incremented Last Symbol Position when consuming symbol", function(){
+         var la = new LexicalAnalyser();
+         la.setText("(a|b);c");
+         la.consumeSymbol();
+         expect(la.getLastSymbolPosition()).toEqual(1);
+         la.consumeSymbol();
+         la.consumeSymbol();
+         la.consumeSymbol();
+         expect(la.getLastSymbolPosition()).toEqual(4);
+      });
+
+      it("It should return a corresponding Symbol in text pointed by las symbol counter when consuming symbol", function(){
+         var la = new LexicalAnalyser();
+         la.setText("(a|b);c");
+         expect(la.consumeSymbol()).toEqual("(");
+         expect(la.consumeSymbol()).toEqual("a");
+         expect(la.consumeSymbol()).toEqual("|");
+      });
+
+      //it("",function(){});
 });
+
--- src/LexicalAnalyser.js
+++ src/LexicalAnalyser.js
@@ -1,30 +1,55 @@
 function LexicalAnalyser () {
    this.machine = null;
+   this.text = '';
+   this.firstSymbolPosition = 0;
+   this.lastSymbolPosition = 0;
 };
 
 LexicalAnalyser.prototype.getMachine = function (){
    return this.machine;
 };
 LexicalAnalyser.prototype.setMachine = function (machine){
-   if(!machine instanceof FiniteStateMachine){
+   if(!machine.addState||!machine.isMatchString){
       throw new TypeError("Machine should be an FiniteStateMachine object!");
    }
    this.machine = machine;
 };
 
 LexicalAnalyser.prototype.isLexicalCorrect = function (text){
-   try{
-      this.machine.isMatchString(text);
-   } catch(e){
-      console.log("catch");
-      console.log(this.machine.getCurrentState());
-      console.log(this.machine.getCurrentSymbolCounter());
-      console.log(text);
-      console.log('"'+text.substr(this.machine.getCurrentSymbolCounter()-1, text.length)+'"');
-      return this.isLexicalCorrect(text.substr(this.machine.getCurrentSymbolCounter()-1, text.length));
+   this.machine.setCurrentState(this.machine.getInitialState());
+   var i = 0;
+   var c = text[i];
+   while(i<text.length){
+      this.machine.moveToNextState(c);
+      c = text[++i];
    }
-      console.log(this.machine.getCurrentState());
-      console.log(this.machine.getCurrentSymbolCounter());
-   return true;
-}
+   return this.machine.isAFinalState(this.machine.setCurrentState());
+};
 
+LexicalAnalyser.prototype.getText = function (){
+   return this.text;
+};
+
+LexicalAnalyser.prototype.setText = function (newText){
+   return this.text = newText;
+};
+
+LexicalAnalyser.prototype.setFirstSymbolPosition = function (newFirstPos){
+   if(newFirstPos<0 || this.text.length<newFirstPos){
+      throw new RangeError("First symbol should be beetween 0 and "+this.text.length-1+"!");
+   }
+   this.firstSymbolPosition = newFirstPos;
+   this.lastSymbolPosition = this.firstSymbolPosition;
+};
+
+LexicalAnalyser.prototype.getFirstSymbolPosition = function (){
+   return this.firstSymbolPosition;
+};
+
+LexicalAnalyser.prototype.getLastSymbolPosition = function (){
+   return this.lastSymbolPosition;
+};
+
+LexicalAnalyser.prototype.consumeSymbol = function (){
+   return this.text[this.lastSymbolPosition++];
+};
