4884ba58ebc86ea30f5ccf2536dee172276f520f
Igor Knop <igorknop@gmail.com>
2011-11-08 16:36:10 -0200

    Adding tests and some fixes for Lexer

--- spec/LexerSpec.js
+++ spec/LexerSpec.js
@@ -6,5 +6,44 @@ describe("A adhoc lexical analiser", function(){
         expect(lex.words["true"]).toEqual(Tag.TRUE);
         expect(lex.words["false"]).toEqual(Tag.FALSE);
     });
+	it("Should increment line after scan a \n", function(){
+		var lex = new Lexer();
+		lex.setText("\n");
+		lex.scan();
+		expect(2).toEqual(lex.currentLine());
+		lex.setText("\n\n\n");
+		lex.scan();
+		expect(4).toEqual(lex.currentLine());
+	});
+	it("Should return 123 after scan a '123'", function(){
+		var lex = new Lexer();
+		lex.setText("123");
+		var o = lex.scan();
+		expect({tag: Tag.NUM, value:123}).toEqual(o);
+	});
+	it("Should return 'name' after scan a 'name'", function(){
+		var lex = new Lexer();
+		lex.setText("name");
+		var o = lex.scan();
+		expect({tag: Tag.ID, lexeme:"name"}).toEqual(o);
+	});
+	it("Should return true after scan a ' true'", function(){
+		var lex = new Lexer();
+		lex.setText(" true");
+		var o = lex.scan();
+		expect(Tag.TRUE).toEqual(o);
+	});	
+	it("Should return false, 321, 'test' and true after scan a 'false 321 test true'", function(){
+		var lex = new Lexer();
+		lex.setText("false 321 test true");
+		var o = lex.scan();
+		expect(Tag.FALSE).toEqual(o);
+		o = lex.scan();
+		expect({tag: Tag.NUM, value:321}).toEqual(o);
+		o = lex.scan();
+		expect({tag: Tag.ID, lexeme:"test"}).toEqual(o);
+		o = lex.scan();
+		expect(Tag.TRUE).toEqual(o);
+	});
     
 });
--- src/Lexer.js
+++ src/Lexer.js
@@ -9,12 +9,23 @@ function Lexer(){
     this.reserve(new Word(Tag.FALSE, "false"));
 }
 
+Lexer.prototype.setText = function(text){
+    this.line = 1;
+    this.peek = " ";
+    this.words = {};
+    this.currentPosition = 0;
+    this.text = text;
+    
+    this.reserve(new Word(Tag.TRUE, "true"));
+    this.reserve(new Word(Tag.FALSE, "false"));
+}
+
 Lexer.prototype.reserve = function(word){
     this.words[word.lexeme] = word.tag;
 };
 
 Lexer.prototype.scan = function() {
-    for (;; this.peek = this.text[i++]) {
+    for (;; this.peek = this.text[this.currentPosition++]) {
         if (this.peek === " " || this.peek === "\t") {
             continue;
         }
@@ -23,21 +34,21 @@ Lexer.prototype.scan = function() {
         }
         else break;
     }
-    if("0".charCodeAt(0)<=this.peek && this.peek <= "9".charCodeAt(0)){
+    if(this.isDigit(this.peek)){
         var v = 0;
         do {
             v = 10*v+parseInt(this.peek,10);
-            this.peek = this.text[++this.currentPosition];
-        }while("0".charCodeAt(0)<=this.peek && this.peek <= "9".charCodeAt(0));
+            this.peek = this.text[this.currentPosition++];
+        }while(this.isDigit(this.peek));
+		return new Num(v);
     }
-    if("A".charCodeAt(0)<=this.peek && this.peek <= "z".charCodeAt(0)){
+    if(this.isLetter(this.peek)){
         var b = "";
         do {
-            b = b+(this.peek);
-            this.peek = this.text[++this.currentPosition];
+            b = b + this.peek;
+            this.peek = this.text[this.currentPosition++];
         }
-        while (("A".charCodeAt(0) <= this.peek && this.peek <= "z".charCodeAt(0)) ||
-        ("0".charCodeAt(0) <= this.peek && this.peek <= "9".charCodeAt(0)));
+        while (this.isLetter(this.peek) || this.isDigit(this.peek));
         w = this.words[b];
         if(w) return w;
         w = new Word(Tag.ID, b);
@@ -49,3 +60,14 @@ Lexer.prototype.scan = function() {
     return t;
 };
 
+Lexer.prototype.currentLine = function(){
+	return this.line;
+}
+
+Lexer.prototype.isDigit = function(c){
+	return (c!==undefined) && ("0".charCodeAt(0)<=c.charCodeAt(0)) && (c.charCodeAt(0) <= "9".charCodeAt(0));
+}
+
+Lexer.prototype.isLetter = function(c){
+	return (c!==undefined) && ("A".charCodeAt(0)<=c.charCodeAt(0)) && (c.charCodeAt(0) <= "z".charCodeAt(0));
+}
