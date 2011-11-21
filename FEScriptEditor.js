11388b6ff9dd10005772b383701b66c24581e2a7
Igor Knop <igorknop@gmail.com>
2011-11-21 02:13:19 -0200

    Adding a UI form for FEScript

---
+++ FEScriptEditor.js
@@ -0,0 +1,53 @@
+window.addEventListener("load", init, false);
+
+function init() {
+	console.info("Initializing editor");
+	loadScript([
+		"Tag.js",
+		"Token.js",
+		"FEScriptLexer.js",
+		"FEScriptParser.js"
+	]);
+
+	var btnRun = document.getElementById("fscrun");
+	btnRun.addEventListener("click", btnrunClick, false);
+	
+}
+
+function loadScript(scripts){
+	var head = document.getElementsByTagName("head")[0];
+	var script;
+	for(var s in scripts){
+		script = document.createElement("script");
+		script.setAttribute("src","src/"+scripts[s]);
+		head.appendChild(script);
+	}
+}
+
+function btnrunClick(){
+	if(event.preventDefault){
+		event.preventDefault();
+	}
+	event.result = false;
+	var parser = new FEScriptParser();
+	var lexer = new FEScriptLexer();
+	parser.setLexer(lexer);
+	console.info("Parsing: "+document.fscripteditor.fsctext.value);
+	parser.setText(document.fscripteditor.fsctext.value);
+	try {
+		parser.parse();
+	} catch (e){
+		var errors = document.getElementById("errors");
+		var error = document.createElement("dt");
+		error.innerHTML = new Date()+": "+e.toLocaleString();
+		error.setAttribute("class","error");
+		errors.appendChild(error);
+		return;
+	}
+	var errors = document.getElementById("errors");
+	var error = document.createElement("dt");
+	error.innerHTML = new Date()+": OK";
+	error.setAttribute("class","ok");
+	errors.appendChild(error);
+	
+}
